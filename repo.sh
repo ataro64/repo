c68597180b647a2026de4931ad198cb85dba8c59
jroemer551@gmail.com <jroemer551@gmail.com>
2021-08-24 13:18:19 -0400

    Added new package

---
+++ repo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+script_full_path=$(dirname "$0")
+cd $script_full_path || exit 1
+
+rm Packages Packages.bz2 Packages.xz Packages.zst Release
+
+apt-ftparchive packages ./pool > Packages
+zstd -q -c19 Packages > Packages.zst
+xz -c9 Packages > Packages.xz
+bzip2 -c9 Packages > Packages.bz2
+
+echo "[Repository] Generating Release..."
+apt-ftparchive \
+        -o APT::FTPArchive::Release::Origin="Sileo" \
+        -o APT::FTPArchive::Release::Label="Sileo" \
+        -o APT::FTPArchive::Release::Suite="stable" \
+        -o APT::FTPArchive::Release::Version="2.0" \
+        -o APT::FTPArchive::Release::Codename="ios" \
+        -o APT::FTPArchive::Release::Architectures="iphoneos-arm" \
+        -o APT::FTPArchive::Release::Components="main" \
+        -o APT::FTPArchive::Release::Description="Sileo for Checkra1n and Unc0ver" \
+        release . > Release
